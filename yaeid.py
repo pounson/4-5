65af5cd3450296aee5c2ab97d8e5568412efb823
pounson <fakhradg@gmail.com>
2022-01-27 19:24:22 +0300

    first commit

---
+++ yaeid.py
@@ -0,0 +1,29 @@
+nested_list = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f','h',False],
+    [1, 2, None],
+]
+
+class FlatIterator:
+    def __init__(self, lst):
+        self.lst = lst
+        self.cursor = -1
+        self.nest_cursor = 0
+        self.list_len = len(self.lst)
+
+    def __iter__(self):
+        self.cursor += 1
+        self.nest_cursor = 0
+        return self
+
+    def __next__(self):
+        while self.cursor - self.list_len and self.nest_cursor == len(self.lst[self.cursor]):
+          iter(self)
+        if self.cursor == self.list_len:
+          raise StopIteration
+        self.nest_cursor += 1
+        return self.lst[self.cursor][self.nest_cursor - 1]
+
+if __name__ == '__main__':
+  flat_list = [item for item in FlatIterator(nested_list)]
+  print(flat_list)
